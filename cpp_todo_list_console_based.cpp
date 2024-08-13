5ce3e36bf2414b66baa525756dd05ba9b7dee252
Ali Eljerrari <dev@coding-dynasty.xyz>
2024-08-13 18:49:09 +0100

    initial commit

---
+++ cpp_todo_list_console_based.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "cpp_todo_list_console_based.h"
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+    size_t agesArraySize = 2;
+
+    int ages[agesArraySize] = { 27, 30 };
+
+
+    cout << "My age is: " << calcAddition(ages[0], ages[1]) << endl;
+    return 0;
+}
+
+int calcAddition(int x, int y) {
+    return x + y;
+}
---
+++ cpp_todo_list_console_based.h
@@ -0,0 +1,11 @@
+/// @brief 
+/// @param argc arguments count
+/// @param argv arguments array value
+/// @return 0 - success | 1 - error
+int main(int argc, char* argv[]);
+
+/// @brief 
+/// @param x first argument
+/// @param y second argument
+/// @return result of x + y
+int calcAddition(int x, int y);
